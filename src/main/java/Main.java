c09afbb09330950163fbe2e69c49cecefdeef0e7
Athena Ballensky <athenaballenskyschool@gmail.com>
2025-09-08 19:58:48 -0700

    Added main, replaced RandomAIPlayer with AIPlayer
    Added main, replaced RandomAIPlayer with AIPlayer to be consistent with Jamshidi's document outline

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        Player human = new HumanPlayer("You", Mark.X, in);
+        Player ai = new AIPlayer("Computer", Mark.O, new RandomStrategy());
+
+        Game game = new Game(human, ai);
+        game.play();
+    }
+}
