db1b1c0cda3a101e3027ab941af811a66f8a7341
Athena Ballensky <athenaballenskyschool@gmail.com>
2025-09-09 17:50:56 -0700

    Added JUnit Testing

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,2 +1,56 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BoardTest {
+
+    //makes sure board is empty at the start of the game
+    @Test
+    void testBoardStartsEmpty()
+    {
+        Board board = new Board();
+        assertTrue(board.isEmpty(0,0));
+        assertTrue(board.isEmpty(0,0));
+    }
+
+    //makes sure the marks actually appear on the board when placed
+    @Test
+    void testMarkPlaced()
+    {
+        Board board = new Board();
+        assertTrue(board.place(0, 0, Mark.X));
+        assertFalse(board.isEmpty(0, 0));
+    }
+
+    //makes sure the correct mark (X or O) appears on the board when placed
+    @Test
+    void testCorrectMarkPlaced()
+    {
+        Board board = new Board();
+        board.place(0, 0, Mark.X);
+        assertFalse(board.place(0, 0, Mark.O));
+    }
+
+    void testWinnerInRow()
+    {
+        Board board = new Board();
+        board.place(0, 0, Mark.X);
+        board.place(1, 0, Mark.X);
+        board.place(2, 0, Mark.X);
+        assertEquals(Mark.X, board.winner());
+    }
+
+    //tests if board is full after placing marks in every spot on the grid
+    @Test
+    void testBoardEndsFull()
+    {
+        Board board = new Board();
+        for (int r = 0; r < 3; r++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                board.place(r, c, Mark.O);
+            }
+        }
+        assertTrue(board.isFull());
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,58 @@
-public class GameTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest
+{
+    static class FixedMoveStrategy implements MoveStrategy
+    {
+        private final int[][] moves;
+        private int index = 0;
+
+        FixedMoveStrategy(int[][] moves)
+        {
+            this.moves = moves;
+        }
+
+        @Override
+        public int[] pickMove(Board board, Mark mark)
+        {
+            return moves[index++];
+        }
+    }
+
+    @Test
+    void testGameAlternatesPlayers()
+    {
+        Player p1 = new AIPlayer("P1", Mark.X, new FixedMoveStrategy(
+                new int[][] { {0,0}, {1,0}, {2,0} } ));
+        Player p2 = new AIPlayer("P2", Mark.O, new FixedMoveStrategy(
+                new int[][] { {0,1}, {1,1}, {2,1} } ));
+
+        Game game = new Game(p1, p2);
+        // simulate play manually, just test moves alternate
+        int[] firstMove = p1.chooseMove(new Board());
+        int[] secondMove = p2.chooseMove(new Board());
+        assertNotEquals(firstMove, secondMove);
+    }
+
+    @Test
+    void testWinningGameEnds()
+    {
+        Player p1 = new AIPlayer("P1", Mark.X, new FixedMoveStrategy(
+                new int[][] { {0,0}, {0,1}, {0,2} } ));
+        Player p2 = new AIPlayer("P2", Mark.O, new FixedMoveStrategy(
+                new int[][] { {1,0}, {1,1} } ));
+
+        Game game = new Game(p1, p2);
+        Board board = new Board();
+
+        // Simulate moves (skip printing)
+        board.place(0,0,Mark.X);
+        board.place(1,0,Mark.O);
+        board.place(0,1,Mark.X);
+        board.place(1,1,Mark.O);
+        board.place(0,2,Mark.X);
+
+        assertEquals(Mark.X, board.winner());
+    }
 }
